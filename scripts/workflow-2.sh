5d0ec33ab08160fc28c164d130419be850f5134a
rahulbshrestha <ra.shrestha@jacobs-university.de>
2020-11-16 15:27:15 +0100

    Updating workflow-2 script

--- scripts/workflow-2.sh
+++ scripts/workflow-2.sh
@@ -8,9 +8,10 @@ if [ "$1" = "-m" ]; then
   make && make install
 fi
 
-# Generate solution from mission with default config
+# Generate solution from mission with default config, runs for set time amount
+
 #./src/templ-transport_network_planner --mission ~/templ-dev/planning/templ/test/data/scenarios/should_succeed/0.xml --min_solutions 1 --configuration ~/templ-dev/planning/templ/test/data/configuration/default-configuration.xml
-./src/templ-transport_network_planner --mission ~/thesis/data/missions/mission-1/mission-1.xml --min_solutions 50 --configuration ~/thesis/data/missions/configuration.xml
+timeout 15m ./src/templ-transport_network_planner --mission ~/thesis/data/missions/mission-1/mission-1.xml --min_solutions 75 --configuration ~/thesis/data/missions/configuration.xml
 
 # Find the latest directory generated; move solution_analysis to new directory 
 SOLUTIONDIR=$(ls /tmp/ | grep templ | sort | tail -1)
